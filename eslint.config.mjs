979ad0736ef3fc0648e5f5801d2ff2fb3eb6286e
KamiKruse <altsake@protonmail.com>
2025-09-09 19:34:37 +0530

    edit eslint to use simple import sort plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,56 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            [
+              // Side effect imports.
+              '^\\u0000',
+              // Node.js builtins prefixed with `node:`.
+              '^node:',
+              // Packages.
+              // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+              '^@?\\w',
+              // Absolute imports and other imports such as Vue-style `@/foo`.
+              // Anything not matched in another group.
+              '^[^.]',
+              // Relative imports.
+              // Anything that starts with a dot.
+              '^\\.',
+            ],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
