081808f95df9ffb8fea5c0686076d372768d2009
KamiKruse <altsake@protonmail.com>
2026-01-06 12:48:54 +0530

    complete baseUrl with logic for baseurl, vercel url and vercel server url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,11 @@
 const getBaseUrls = () => {
-  if(process.env.NEXT_PUBLIC_VERCEL_URL){
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return `https://${process.env.NEXT_PUBLIC_BASE_URL}`
+  }
+  if (process.env.VERCE_URL) {
+    return `https://${process.env.VERCE_URL}`
+  }
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   }
   return 'http://localhost:3000'
